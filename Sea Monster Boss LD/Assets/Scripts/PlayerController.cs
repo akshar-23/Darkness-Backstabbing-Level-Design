02ad7c30a158babaa0164d6f3a3db33e6ef9f01c
Lincoln Freedman <55606828+lincolnfreedman@users.noreply.github.com>
2025-09-30 21:40:30 -0400

    implemented character controller

---
+++ Sea Monster Boss LD/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework.Internal;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    PlayerInput input;
+    Vector2 moveInput;
+    [SerializeField] Camera playerCamera;
+
+    [SerializeField] float moveSpeed = 0.1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        input = GetComponent<PlayerInput>();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        moveInput = input.actions["Move"].ReadValue<Vector2>();
+
+        Vector3 camForward = playerCamera.transform.forward;
+        Vector3 camRight = playerCamera.transform.right;
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
+
+        Vector3 posChange = camRight * moveInput.x + camForward * moveInput.y;
+        transform.position += posChange * moveSpeed;
+
+        transform.rotation = Quaternion.Euler(0, playerCamera.transform.eulerAngles.y, 0);
+
+    }
+}
